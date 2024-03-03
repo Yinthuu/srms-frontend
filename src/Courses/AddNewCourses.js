8f2dba72b4168f5d16dc5ab49ee340c245a83e23
Yin ThuAye <yin.thu.aye0@gmail.com>
2024-03-02 22:20:54 -0500

    Courses- Add courses pages

--- src/Courses/AddNewCourses.js
+++ src/Courses/AddNewCourses.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 export default function AddNewCourses() {
+
+  const [course, setCourse]=useState({
+    coursename:""
+})
+  const {coursename} =  course;
+  const [notification, setNotification] = useState("");
+
+  const onInputChange=(e)=>{
+    setCourse({...course, [e.target.name]:e.target.value})  
+  }
+
+  const onSubmit =async (e)=>{
+    e.preventDefault();
+    try {
+        await axios.post("http://localhost:8080/course", course)
+        //show noti
+        setNotification("A new course is created successfully!");
+        setTimeout(() => {
+            setNotification("");
+        }, 3000);
+        // clear all values
+        setCourse({
+          coursename: ""
+        });
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
+
   return (
-    <div>AddNewCourses</div>
+    <div className="container">
+    <div className="row">
+        <div className="col-md-10 offset-md-1 border rounded p-4 mt-2 shadow">
+        <h2 className="text-center m-4">Add New Course</h2>
+        {notification && <div className="alert alert-success">{notification}</div>}
+        <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+            <label htmlFor="coursename" className="form-label">Course name</label>
+            <input type={"text"} className="form-control" name="coursename" value={coursename} onChange={(e)=>onInputChange(e)} required/>
+            </div>
+
+            <div className="center-button">
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+        </form>
+        </div>
+    </div>
+  </div>
   )
 }
--- src/Courses/CoursesList.js
+++ src/Courses/CoursesList.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { BsX } from 'react-icons/bs';
+import { useParams } from "react-router-dom";
+import '../App.css';
 
 export default function CoursesList() {
+  const [courses,setCourses] = useState([])
+  //id of course to be deleted
+  const {id} = useParams();
+
+  useEffect(()=>{
+    loadCourses();
+  },[])
+
+  const loadCourses=async()=>{
+    const result= await axios.get("http://localhost:8080/courses");
+    setCourses(result.data);
+  }
+
+  //delete course
+  const deleteCourse = async (id) =>{
+    await axios.delete(`http://localhost:8080/course/${id}`)
+    loadCourses();
+  }
+
   return (
-    <div>CoursesList</div>
+    <div className="container">
+      <div className="py-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Course Name</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+                courses.map((course, index) => (
+                    <tr>
+                    <th scope="row" key={index} > {index+1} </th>
+                    <td>{course.coursename}</td>
+                    <td>
+                    <button className='btn' onClick={()=>deleteCourse(course.id)}>
+                        <BsX className="delete-icon"/>
+                    </button>
+                    </td>
+                  </tr>
+                ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
