628fd620bc12a1abb20dfa03d6ad8e046e8b4d45
Yin ThuAye <yin.thu.aye0@gmail.com>
2024-03-03 00:14:35 -0500

    AddNewStudent- check email and date validation

--- src/Students/AddNewStudents.js
+++ src/Students/AddNewStudents.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import validator from 'validator';
 
 export default function AddNewStudents() {
 
@@ -12,13 +13,32 @@ export default function AddNewStudents() {
 
     const {firstname, familyname, dateofbirth, email} =  student;
     const [notification, setNotification] = useState("");
+    const [emailError, setEmailError] = useState("");
 
     const onInputChange=(e)=>{
-      setStudent({...student, [e.target.name]:e.target.value})  
+      setStudent({...student, [e.target.name]:e.target.value});
+      if (e.target.name === 'email') {
+        setEmailError(validator.isEmail(e.target.value) ? "" : "Please enter a valid email address");
+    }
     }
 
     const onSubmit =async (e)=>{
         e.preventDefault();
+        // validate date of birth is atleast 10 years
+        const currentDate = new Date();
+        const birthDate = new Date(dateofbirth);
+        const diffInYears = (currentDate - birthDate) / (1000 * 60 * 60 * 24 * 365);
+        if (diffInYears < 10) {
+            setNotification("");
+            setEmailError("");
+            alert("Date of birth must be at least 10 years old.");
+            return;
+        }
+
+        if (!validator.isEmail(email)) {
+            setEmailError("Please enter a valid email address");
+            return;
+        }
         try {
             await axios.post("http://localhost:8080/student", student)
             //show noti
@@ -46,23 +66,24 @@ export default function AddNewStudents() {
             {notification && <div className="alert alert-success">{notification}</div>}
             <form onSubmit={(e) => onSubmit(e)}>
                 <div className="mb-3">
-                <label htmlFor="Firstname" className="form-label">First name</label>
-                <input type={"text"} className="form-control" name="firstname" value={firstname} onChange={(e)=>onInputChange(e)} required/>
+                <label htmlFor="firstname" className="form-label">First name</label>
+                <input type="text" className="form-control" name="firstname" id="firstname" value={firstname} onChange={(e)=>onInputChange(e)} required/>
                 </div>
 
                 <div className="mb-3">
-                <label htmlFor="Familyname" className="form-label">Family name</label>
-                <input type={"text"} className="form-control" name="familyname" value={familyname} onChange={(e)=>onInputChange(e)} required/>
+                <label htmlFor="familyname" className="form-label">Family name</label>
+                <input type="text" className="form-control" name="familyname" id="familyname" value={familyname}  onChange={(e)=>onInputChange(e)} required/>
                 </div>
 
                 <div className="mb-3">
-                <label htmlFor="Dateofbirth" className="form-label">Date Of Birth</label>
-                <input type={"text"} className="form-control" name="dateofbirth" value={dateofbirth} onChange={(e)=>onInputChange(e)} required/>
+                <label htmlFor="dateofbirth" className="form-label">Date Of Birth</label>
+                <input type="date" className="form-control" name="dateofbirth" id="dateofbirth" value={dateofbirth} onChange={(e)=>onInputChange(e)} required/>
                 </div>
 
                 <div className="mb-3">
-                <label htmlFor="Email" className="form-label">Email</label>
-                <input type={"text"} className="form-control" name="email" value={email} onChange={(e)=>onInputChange(e)} required/>
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" className="form-control" name="email" id="email" value={email} onChange={(e)=>onInputChange(e)} required/>
+                 {emailError && <div className="text-danger">{emailError}</div>}
                 </div>
 
                 <div className="center-button">
